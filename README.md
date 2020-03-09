a68b46bd06c4fe2b14242f5a60ac4f94580b813c
JanCl <jancl@outlook.de>
2020-03-09 21:26:21 +0100

    argparse etc

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# tight vdf
-https://eprint.iacr.org/2018/627.pdf
-https://eprint.iacr.org/2019/659.pdf
+# Simple and Tight Verifiable Delay Functions
+The code in this repository implements the "Simple VDF" of Pietrzak (https://eprint.iacr.org/2018/627.pdf) as well as the "Tight VDF" (by Döttling et al.) application on it (https://eprint.iacr.org/2019/659.pdf) on user specifiable timing parameters. 
+
+## Usage
+To run a simple example comparing both VDFs run 
+'python3 src/protocol/systemtests.py t'
+with T = 2^t as timing parameter
+To execute only the simple VDF run
+'python3 src/protocol/systemtests.py t -s'
+respectively for the tight VDF
+'python3 src/protocol/systemtests.py t -t'
+
+## Comparison table
+To get a table with preset range of t comparing both VDFs run 
+'python3 src/protocol/systemtests.py 0 -c'
+The range can be adjusted in the main function of src/protocol/systemtests.py 
+
+## Notes
+'python3 src/protocol/systemtests.py -h'
+will print a simple usage guide
+
+- The VDFs with t=30 will already take several minutes to be computed. Hence we suggest to not run the implementation with much higher values of t just for demonstration purposes. 
+
+
+
--- src/protocol/systemtests.py
+++ src/protocol/systemtests.py
@@ -1,4 +1,9 @@
 import time
+import argparse
+import os
+import sys
+from tabulate import tabulate
+
 from VDF import *
 from parallel_scheme import *
 
@@ -22,7 +27,7 @@ def evaluate(N, x, T):
 def prov_mod(alpha):
     return alpha
     
-def detailed_output(T, N, x):
+def simple_VDF(T, N, x):
     print("(simple) Solving...")
     start_time = time.time()
     y = comp(N, x, T)
@@ -38,7 +43,7 @@ def detailed_output(T, N, x):
     if (verify(N, x, T, y, pi)):
         print("... Simple output accepted in", time.time() - start_time)
     
-    
+def tight_VDF(T, N, x): 
     
     #parallel = Parallel_scheme( T, setup, gen, evaluate, vf, prov_mod, evaluate)
     parallel = Parallel_scheme( T, setup, gen, evaluate, vf, prov, comp)
@@ -53,8 +58,76 @@ def detailed_output(T, N, x):
     start_time = time.time()
     print(parallel.vf_(N, x, y_parallel, pi_parallel, T), "in", time.time() - start_time)
 
-T = 2 ** 25
-N = setup(64)
-x = gen(N)
+def complete(t, N, x):
+    print("Computing both VDF schemes on input 2^", t, "=", 2**t)
+    simple_VDF(2 ** t, N, x)
+    tight_VDF(2 ** t, N, x)
+    
+def simple(t, N, x):
+    print("Computing simple VDF scheme on input 2^", t, "=", 2**t)
+    simple_VDF(2 ** t, N, x)
+    
+def tight(t, N, x):
+    print("Computing tight VDF scheme on input 2^", t, "=", 2**t)
+    tight_VDF(2 ** t, N, x)
 
-detailed_output(T, N, x)
+def table(t1, t2, N, x):
+    metrics = ["t", "simple evaluate", "tight evaluate", "simple verify", "tight verify"]
+    values = list()
+    for i in range (t1, t2 + 1):
+        T = 2 ** i
+        row = [i]
+        start_time = time.time()
+        y = comp(N, x, T)
+        pi = prov(N, x, T, y)
+        row.append(time.time() - start_time)
+        parallel = Parallel_scheme( T, setup, gen, evaluate, vf, prov, comp)
+        start_time = time.time()
+        y_parallel, pi_parallel = parallel.eval_(N, x, T)
+        row.append(time.time() - start_time)
+        start_time = time.time()
+        if (verify(N, x, T, y, pi)):
+            row.append(time.time() - start_time)
+        else:
+            print("Simple verification failed")
+            sys.exit()
+
+        start_time = time.time()
+        if (parallel.vf_(N, x, y_parallel, pi_parallel, T) == "Parallel output accepted"):
+            row.append(time.time() - start_time)
+        else:
+            print("Parallel verification failed")
+            sys.exit()
+        values.append(row)
+    print(tabulate(values, headers = metrics, tablefmt="grid"))
+    
+if __name__ == "__main__":
+    t = 15          # default value
+    t_start = 15
+    t_end = 27
+    N = setup(64)
+    x = gen(N)
+    max_t = 1000
+                    # argparse section
+    vdf_parser = argparse.ArgumentParser(description='Simple and tight VDF implementation')
+    vdf_parser.add_argument('t_parameter', metavar='t', type=int, help='Timing parameter t -> using T=2^t')
+    vdf_parser.add_argument('-s', '--simple', action='store_true', help="just compute simple VDF")
+    vdf_parser.add_argument('-t', '--tight', action='store_true', help="just compute tight (parallel) VDF")
+    vdf_parser.add_argument('-c', '--compare', action='store_true', help="compares running time of both schemes with different inputs")
+    args = vdf_parser.parse_args()
+    input_t = args.t_parameter
+    if args.compare:
+        table(t_start, t_end, N, x);
+        sys.exit()
+    if (input_t > 1 and input_t < max_t):
+        t = int(input_t)
+    else :
+        print("given t out of range ( 1 -", max_t, ") hence using default t =", t)
+    if args.simple:
+        simple(t, N, x)
+    if args.tight:
+        tight(t, N, x)
+    if not(args.simple or args.tight):
+        complete(t, N, x)
+    
+    
